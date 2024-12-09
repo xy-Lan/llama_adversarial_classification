16f0f169a61ae84790e7f4b96ef4ddfe0e1d09f2
Xingyu Lan <xlan7@sheffield.ac.uk>
2024-12-09 07:45:59 +0000

    new version

--- scripts/classify_adversarial.py
+++ scripts/classify_adversarial.py
@@ -93,7 +93,7 @@ def construct_prompts(df):
     return original_prompts, adversarial_prompts, skipped_samples, valid_samples
 
 
-def load_model(model_name="meta-llama/Llama-3.1-8B-Instruct", token="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"):
+def load_model(model_name="Llama-3.2-11B-Vision-Instruct", token="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"):
     # tokenizer = LlamaTokenizer.from_pretrained(model_dir)
     # model = LlamaForCausalLM.from_pretrained(model_dir)
     # model.eval()
