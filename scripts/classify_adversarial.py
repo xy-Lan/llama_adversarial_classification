2ae7131fbdf4384b3c688437bbcb83b460e6a53b
Xingyu Lan <xlan7@sheffield.ac.uk>
2025-04-12 09:47:12 +0100

    尝试11B

--- scripts/classify_adversarial.py
+++ scripts/classify_adversarial.py
@@ -13,7 +13,8 @@ from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 
 def load_data(file_path):
     # 加载第401行到最后一行
-    df = pd.read_csv(file_path, skiprows=list(range(1, 401)))
+    # df = pd.read_csv(file_path, skiprows=list(range(1, 401)))
+    df = pd.read_csv(file_path, nrows=300)
     return df
 
 
@@ -93,7 +94,7 @@ def construct_prompts(df):
     return original_prompts, adversarial_prompts, skipped_samples, valid_samples
 
 
-def load_model(model_name="meta-llama/Meta-Llama-3-8B-Instruct", token="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"):
+def load_model(model_name="meta-llama/Llama-3.2-11B-Vision-Instruct", token="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"):
     # tokenizer = LlamaTokenizer.from_pretrained(model_dir)
     # model = LlamaForCausalLM.from_pretrained(model_dir)
     # model.eval()
@@ -172,15 +173,15 @@ def compare_results(df, original_predictions, adversarial_predictions, valid_sam
 
     # 导出成功翻转的样本到CSV文件
     flipped_samples_df = df[df['prediction_flipped'] == True]
-    flipped_samples_df.to_csv('./data/Llama-3.2-8B-Instruct_flipped_samples_2.csv', index=False)
+    flipped_samples_df.to_csv('./data/meta-llama/Llama-3.2-11B-Vision-Instruct_1.csv', index=False)
     print(
-        f"Successfully exported {len(flipped_samples_df)} flipped samples to './data/Llama-3.2-8B-Instruct_flipped_samples_2.csv'")
+        f"Successfully exported {len(flipped_samples_df)} flipped samples to './data/Llama-3.2-11B-Vision-Instruct_1.csv'")
 
     # 导出保留原义且成功翻转的样本到CSV文件
     preserved_flipped_df = df[(df['agreed_labels'] == 0) & (df['prediction_flipped'] == True)]
-    preserved_flipped_df.to_csv('./data/Llama-3.2-8B-Instruct_preserved_flipped_samples_2.csv', index=False)
+    preserved_flipped_df.to_csv('./data/Llama-3.2-11B-Vision-Instruct_preserved_flipped_samples_1.csv', index=False)
     print(
-        f"Successfully exported {len(preserved_flipped_df)} preserved meaning flipped samples to './data/Llama-3.2-8B-Instruct_preserved_flipped_samples_2.csv'")
+        f"Successfully exported {len(preserved_flipped_df)} preserved meaning flipped samples to './data/Llama-3.2-11B-Vision-Instruct_preserved_flipped_samples_1.csv'")
 
     # 输出结果
     print(f"Total samples: {total_samples}")
