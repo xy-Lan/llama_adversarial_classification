c10a6f4aa918e106dae67563f3ccd7080711bcca
Xingyu Lan <xlan7@sheffield.ac.uk>
2025-04-13 21:32:49 +0100

    gpt classification

--- scripts/classify_adversarial.py
+++ scripts/classify_adversarial.py
@@ -13,8 +13,8 @@ from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 
 def load_data(file_path):
     # 加载第401行到最后一行
-    # df = pd.read_csv(file_path, skiprows=list(range(1, 401)))
-    df = pd.read_csv(file_path, nrows=100)
+    df = pd.read_csv(file_path, skiprows=list(range(1, 101)))
+    # df = pd.read_csv(file_path, nrows=100)
     return df
 
 
@@ -173,15 +173,15 @@ def compare_results(df, original_predictions, adversarial_predictions, valid_sam
 
     # 导出成功翻转的样本到CSV文件
     flipped_samples_df = df[df['prediction_flipped'] == True]
-    flipped_samples_df.to_csv('./data/Llama-3.2-11B-Vision-Instruct_1.csv', index=False)
+    flipped_samples_df.to_csv('./data/Llama-3.2-11B-Vision-Instruct_2.csv', index=False)
     print(
-        f"Successfully exported {len(flipped_samples_df)} flipped samples to './data/Llama-3.2-11B-Vision-Instruct_1.csv'")
+        f"Successfully exported {len(flipped_samples_df)} flipped samples to './data/Llama-3.2-11B-Vision-Instruct_2.csv'")
 
     # 导出保留原义且成功翻转的样本到CSV文件
     preserved_flipped_df = df[(df['agreed_labels'] == 0) & (df['prediction_flipped'] == True)]
-    preserved_flipped_df.to_csv('./data/Llama-3.2-11B-Vision-Instruct_preserved_flipped_samples_1.csv', index=False)
+    preserved_flipped_df.to_csv('./data/Llama-3.2-11B-Vision-Instruct_preserved_flipped_samples_2.csv', index=False)
     print(
-        f"Successfully exported {len(preserved_flipped_df)} preserved meaning flipped samples to './data/Llama-3.2-11B-Vision-Instruct_preserved_flipped_samples_1.csv'")
+        f"Successfully exported {len(preserved_flipped_df)} preserved meaning flipped samples to './data/Llama-3.2-11B-Vision-Instruct_preserved_flipped_samples_2.csv'")
 
     # 输出结果
     print(f"Total samples: {total_samples}")
