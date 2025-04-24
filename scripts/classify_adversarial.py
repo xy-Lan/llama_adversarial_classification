cc28a4ea69eeca632869b58b5c30d5397a592da2
Xingyu Lan <xlan7@sheffield.ac.uk>
2025-04-24 03:06:34 +0100

    new

--- scripts/classify_adversarial.py
+++ scripts/classify_adversarial.py
@@ -172,17 +172,17 @@ def compare_results(df, original_predictions, adversarial_predictions, valid_sam
     flipped_preserve_samples = df_preserve['prediction_flipped'].sum()  # 保留原义中翻转的样本数量
     similarity_weighted_flip_rate = flipped_preserve_samples / valid_samples if valid_samples else 0
 
-    # 导出成功翻转的样本到CSV文件
-    flipped_samples_df = df[df['prediction_flipped'] == True]
-    flipped_samples_df.to_csv('./data/Llama-3.2-1B-Instruct_flipped_samples.csv', index=False)
-    print(
-        f"Successfully exported {len(flipped_samples_df)} flipped samples to './data/Llama-3.2-1B-Instruct_flipped_samples.csv'")
-
-    # 导出保留原义且成功翻转的样本到CSV文件
-    preserved_flipped_df = df[(df['agreed_labels'] == 0) & (df['prediction_flipped'] == True)]
-    preserved_flipped_df.to_csv('./data/Llama-3.2-1B-Instruct_preserved_flipped_samples.csv', index=False)
-    print(
-        f"Successfully exported {len(preserved_flipped_df)} preserved meaning flipped samples to './data/Llama-3.2-1B-Instruct_preserved_flipped_samples.csv'")
+    # # 导出成功翻转的样本到CSV文件
+    # flipped_samples_df = df[df['prediction_flipped'] == True]
+    # flipped_samples_df.to_csv('./data/Llama-3.2-1B-Instruct_flipped_samples.csv', index=False)
+    # print(
+    #     f"Successfully exported {len(flipped_samples_df)} flipped samples to './data/Llama-3.2-1B-Instruct_flipped_samples.csv'")
+    #
+    # # 导出保留原义且成功翻转的样本到CSV文件
+    # preserved_flipped_df = df[(df['agreed_labels'] == 0) & (df['prediction_flipped'] == True)]
+    # preserved_flipped_df.to_csv('./data/Llama-3.2-1B-Instruct_preserved_flipped_samples.csv', index=False)
+    # print(
+    #     f"Successfully exported {len(preserved_flipped_df)} preserved meaning flipped samples to './data/Llama-3.2-1B-Instruct_preserved_flipped_samples.csv'")
 
     # 输出结果
     print(f"Total samples: {total_samples}")
